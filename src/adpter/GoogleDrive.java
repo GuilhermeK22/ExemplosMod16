3f137ffd7ea718411c8589e636f91463696e721a
Guilherme <guilhermekappaun@gmail.com>
2023-09-10 21:50:54 -0300

    Initial commit

---
+++ src/adpter/GoogleDrive.java
@@ -0,0 +1,2 @@
+package adpter;public class GoogleDrive {
+}
---
+++ src/adpter/IPersistenciaArquivos.java
@@ -0,0 +1,9 @@
+package adpter;
+
+import java.io.File;
+
+public interface PersistenciaArquivos {
+    //byte[] bytes = Files.readAllBytes(file.toPath());
+    public void gravar(File file);
+    public File ler(String caminho);
+}
